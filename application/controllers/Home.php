7c2cb29f86fe1a164a84fa2837c11568d62e4406
MrAlligator <55204773+MrAlligator@users.noreply.github.com>
2021-10-25 03:27:50 +0700

    frontend (belum fix)

---
+++ application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Home extends CI_Controller
+{
+    public function index()
+    {
+        $this->load->view('_partials/header');
+        $this->load->view('_partials/hero');
+        $this->load->view('frontend/about');
+        $this->load->view('_partials/footer');
+    }
+}
