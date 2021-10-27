52d1c75116f94b6e089774e7a18fc3e1156747c9
ZL XXIV <55338602+zl-xxiv@users.noreply.github.com>
2021-10-27 19:58:59 +0700

    Update topbar.php

--- application/views/_partials/topbar.php
+++ application/views/_partials/topbar.php
@@ -9,28 +9,28 @@
         <nav id="navbar" class="navbar">
             <ul>
                 <li><a href="<?= base_url('home') ?>">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#team">Team</a></li>
                 <li class="dropdown"><a href="#"><span>Our Data</span> <i class="bi bi-chevron-down"></i></a>
                     <ul>
                         <li><a href="<?= base_url('frontend/buyers') ?>">Buyers</a></li>
                         <li><a href="#">Inquiry</a></li>
-                        <li><a href="#">Drop Down 3</a></li>
-                        <li><a href="#">Drop Down 4</a></li>
-                        <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                            <ul>
+                    </ul>
+                <!-- <li><a href="#about">About</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#team">Team</a></li> -->
+                        <!-- <li><a href="#">Drop Down 3</a></li> -->
+                        <!-- <li><a href="#">Drop Down 4</a></li> -->
+                        <!-- <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a> -->
+                            <!-- <ul>
                                 <li><a href="#">Deep Drop Down 1</a></li>
                                 <li><a href="#">Deep Drop Down 2</a></li>
                                 <li><a href="#">Deep Drop Down 3</a></li>
                                 <li><a href="#">Deep Drop Down 4</a></li>
                                 <li><a href="#">Deep Drop Down 5</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+                            </ul> -->
+                        <!-- </li> -->
                 </li>
-                <li><a href="#contact">Contact</a></li>
+                <!-- <li><a href="#contact">Contact</a></li> -->
                 <?php if (isset($user['email'])) : ?>
                     <li class="dropdown"><a href="#"><img class="img-profile rounded-circle img-thumbnail" width="40" src="<?= base_url('assets/img/userimage/') . $user['foto_user']; ?>"><i class="bi bi-chevron-down"></i></a>
                         <ul>
