28989cc63e68b952f55393200be99e46f752aa1c
MrAlligator <55204773+MrAlligator@users.noreply.github.com>
2021-11-10 18:43:25 +0700

    Update Membership.php

--- application/controllers/frontend/Membership.php
+++ application/controllers/frontend/Membership.php
@@ -8,6 +8,8 @@ class Membership extends CI_Controller
         $data['title'] = "Membership";
         $data['user'] = $this->db->get_where('tb_user', ['email' => $this->session->userdata('email')])->row_array();
 
+        $this->form_validation->set_rules('buktitf', 'Bukti Transfer', 'required');
+
         if ($this->form_validation->run() == false) {
             $this->load->view('_partials/header', $data);
             $this->load->view('_partials/topbar');
@@ -17,6 +19,31 @@ class Membership extends CI_Controller
             $this->load->view('_partials/footer');
             $this->load->view('_partials/js');
         } else {
+            $email = $this->session->userdata('email');
+            $date = time();
+
+            $config['allowed_types'] = 'jpg|png';
+            $config['max_size']         = '2048';
+            $config['upload_path']     = './assets/img/buktitransfer/';
+
+            $this->load->library('upload', $config);
+
+            if ($this->upload->do_upload('buktitf')) {
+                $buktitf = $this->upload->data();
+                $file = $buktitf['file_name'];
+
+                $this->db->set('email', $email);
+                $this->db->set('tanggal_pembayaran', $date);
+                $this->db->set('buktitf', $file);
+
+                $this->db->insert('tb_pembayaran');
+
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Pembayaran Berhasil, Tunggu Konfirmasi Admin..!!</div>');
+                redirect('frontend/membership');
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+                redirect('frontend/membership');
+            }
         }
     }
 }
