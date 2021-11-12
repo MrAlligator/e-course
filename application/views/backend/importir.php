68384dfd72ed35c51b011fd8e92fddeb63408401
MrAlligator <55204773+MrAlligator@users.noreply.github.com>
2021-11-12 10:02:42 +0700

    Update importir.php

--- application/views/backend/importir.php
+++ application/views/backend/importir.php
@@ -7,6 +7,7 @@
                         <div class="col-lg-6 col-7">
                             <h6 class="h2 text-white d-inline-block mb-0"><?= $title ?></h6>
                         </div>
+                        <?php echo $this->session->flashdata('message'); ?>
                     </div>
                 </div>
             </div>
